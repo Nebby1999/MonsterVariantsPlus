04c2d356c3bfb865837e476da9397c2ad1d46761
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 19:49:03 -0400

    First attempt at making a new variant, Steel Contraption.

--- MonsterVariants+/SubClasses/CustomVariants.cs
+++ MonsterVariants+/SubClasses/CustomVariants.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using MonsterVariantsPlus;
+using MonsterVariants.Components;
+using RoR2;
+using UnityEngine;
+using MonsterVariants;
 
 namespace MonsterVariantsPlus.SubClasses
 {
-    class CustomVariants
+    public class CustomVariants
     {
+        internal static void RegisterCustomVariants()
+        {
+            MainPlugin.AddVariant(new MonsterVariants.MonsterVariantInfo //Steel Contraption
+            {
+                bodyName = "Bell",
+                spawnRate = 100,
+                variantTier = MonsterVariants.MonsterVariantTier.Rare,
+                sizeModifier = FlyingSizeModifier(2f),
+                healthMultiplier = 1.5f,
+                moveSpeedMultiplier = 0.7f,
+                attackSpeedMultiplier = 0.5f,
+                damageMultiplier = 2f,
+                armorMultiplier = 1f,
+                armorBonus = 0,
+                customInventory = null,
+                meshReplacement = null,
+                materialReplacement = null,
+                skillReplacement = null
+            });
+        }
+        internal static MonsterSizeModifier FlyingSizeModifier(float newSize)
+        {
+            MonsterSizeModifier sizeModifer = ScriptableObject.CreateInstance<MonsterSizeModifier>();
+            sizeModifer.newSize = newSize;
+            sizeModifer.scaleCollider = true;
+
+            return sizeModifer;
+        }
     }
 }
