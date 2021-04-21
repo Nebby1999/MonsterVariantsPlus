288ac9e931fbbc2b3878e70cba7dc62dc59b0f97
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 21:12:27 -0400

    Semi Copy of Rob's Skills class, has EmptySkillDef

---
+++ MonsterVariants+/SubClasses/Skills/Skills.cs
@@ -0,0 +1,56 @@
+﻿using EntityStates;
+using MonsterVariantsPlus.SubClasses.Skills.CustomSkills;
+using RoR2.Skills;
+using UnityEngine;
+
+namespace MonsterVariantsPlus.SubClasses.Skills
+{
+    internal class Skills
+    {
+        internal static SkillDef emptySkillDef;
+        internal static void RegisterSkills()
+        {
+            Loadouts.AddSkill(typeof(emptySkill));
+
+            emptySkillDef = NewSkillDef(new SerializableEntityStateType(typeof(emptySkill)), "Body");
+
+            emptySkillDef.baseMaxStock = 0;
+            emptySkillDef.requiredStock = 1;
+        }
+        private static SkillDef NewSkillDef(SerializableEntityStateType state, string stateMachine)
+        {
+            return NewSkillDef(state, stateMachine, 1, 0, 0);
+        }
+
+        private static SkillDef NewSkillDef(SerializableEntityStateType state, string stateMachine, int stock, float cooldown, int stockConsumed)
+        {
+            SkillDef skillDef = ScriptableObject.CreateInstance<SkillDef>();
+
+            skillDef.skillName = "SKILL_LUNAR_PRIMARY_REPLACEMENT_NAME";
+            skillDef.skillNameToken = "SKILL_LUNAR_PRIMARY_REPLACEMENT_NAME";
+            skillDef.skillDescriptionToken = "SKILL_LUNAR_PRIMARY_REPLACEMENT_DESCRIPTION";
+            skillDef.icon = null;
+
+            skillDef.activationState = state;
+            skillDef.activationStateMachineName = stateMachine;
+            skillDef.baseMaxStock = stock;
+            skillDef.baseRechargeInterval = cooldown;
+            skillDef.beginSkillCooldownOnSkillEnd = false;
+            skillDef.canceledFromSprinting = false;
+            skillDef.forceSprintDuringState = false;
+            skillDef.fullRestockOnAssign = true;
+            skillDef.interruptPriority = InterruptPriority.Any;
+            skillDef.resetCooldownTimerOnUse = false;
+            skillDef.isCombatSkill = true;
+            skillDef.mustKeyPress = false;
+            skillDef.cancelSprintingOnActivation = true;
+            skillDef.rechargeStock = 1;
+            skillDef.requiredStock = 1;
+            skillDef.stockToConsume = stockConsumed;
+
+            Loadouts.AddSkillDef(skillDef);
+
+            return skillDef;
+        }
+    }
+}
