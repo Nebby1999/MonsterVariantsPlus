fa1ae7232e3cf1eed57d22da983e4f3faa421009
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 22:48:43 -0400

    epic beanz

--- MonsterVariants+/SubClasses/Skills/Skills.cs
+++ MonsterVariants+/SubClasses/Skills/Skills.cs
@@ -11,44 +11,8 @@ namespace MonsterVariantsPlus.SubClasses.Skills
         internal static void RegisterSkills()
         {
             emptySkillDef = Resources.Load<GameObject>("Prefabs/CharacterBodies/CommandoBody").GetComponent<SkillLocator>().primary.skillFamily.variants[0].skillDef;
-
             emptySkillDef.baseMaxStock = 0;
             emptySkillDef.requiredStock = 1;
-        }
-        private static SkillDef NewSkillDef(SerializableEntityStateType state, string stateMachine)
-        {
-            return NewSkillDef(state, stateMachine, 1, 0, 0);
-        }
-
-        private static SkillDef NewSkillDef(SerializableEntityStateType state, string stateMachine, int stock, float cooldown, int stockConsumed)
-        {
-            SkillDef skillDef = ScriptableObject.CreateInstance<SkillDef>();
-
-            skillDef.skillName = "SKILL_LUNAR_PRIMARY_REPLACEMENT_NAME";
-            skillDef.skillNameToken = "SKILL_LUNAR_PRIMARY_REPLACEMENT_NAME";
-            skillDef.skillDescriptionToken = "SKILL_LUNAR_PRIMARY_REPLACEMENT_DESCRIPTION";
-            skillDef.icon = null;
-
-            skillDef.activationState = state;
-            skillDef.activationStateMachineName = stateMachine;
-            skillDef.baseMaxStock = stock;
-            skillDef.baseRechargeInterval = cooldown;
-            skillDef.beginSkillCooldownOnSkillEnd = false;
-            skillDef.canceledFromSprinting = false;
-            skillDef.forceSprintDuringState = false;
-            skillDef.fullRestockOnAssign = true;
-            skillDef.interruptPriority = InterruptPriority.Any;
-            skillDef.resetCooldownTimerOnUse = false;
-            skillDef.isCombatSkill = true;
-            skillDef.mustKeyPress = false;
-            skillDef.cancelSprintingOnActivation = true;
-            skillDef.rechargeStock = 1;
-            skillDef.requiredStock = 1;
-            skillDef.stockToConsume = stockConsumed;
-
-            Loadouts.AddSkillDef(skillDef);
-
-            return skillDef;
         }
     }
 }
