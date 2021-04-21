63cdcd9a0ffe9d32afd3dac8f25db842bd2d7173
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 21:12:15 -0400

    Semi Copy of Rob's Loadout Class

---
+++ MonsterVariants+/SubClasses/Skills/Loadouts.cs
@@ -0,0 +1,20 @@
+﻿using RoR2.Skills;
+using System;
+using System.Collections.Generic;
+
+namespace MonsterVariantsPlus.SubClasses.Skills
+{
+    internal class Loadouts
+    {
+        internal static List<Type> entityStates = new List<Type>();
+        internal static List<SkillDef> skillDefs = new List<SkillDef>();
+        internal static void AddSkill(Type type)
+        {
+            entityStates.Add(type);
+        }
+        internal static void AddSkillDef(SkillDef t)
+        {
+            skillDefs.Add(t);
+        }
+    }
+}
