615bd3d8b1f5d3559e0ae9d8d3adf0a35e792c9f
BuildTools <unconfigured@null.spigotmc.org>
2021-04-29 17:16:18 -0400

    Removed DissapointingFireball since it doesnt work as intended
    Added skill defs related to the unused swarm ability

--- MonsterVariants+/SubClasses/Skills/CustomSkills.cs
+++ MonsterVariants+/SubClasses/Skills/CustomSkills.cs
@@ -13,8 +13,8 @@ namespace MonsterVariantsPlus.SubClasses.Skills
         public static SkillDef hoarderSitDef;
         public static SkillDef xlRecoverDef;
         public static SkillDef kamikazeBlinkDef;
-        public static SkillDef WispAmalgamateDef;
-        public static SkillDef DissapointingFireballDef;
+        public static SkillDef wispAmalgamateDef;
+        //public static SkillDef DeploySwarmDef;
 
         internal static void RegisterSkills()
         {
@@ -23,8 +23,8 @@ namespace MonsterVariantsPlus.SubClasses.Skills
             Loadouts.AddSkill(typeof(States.Scavenger.HoarderSit));
             Loadouts.AddSkill(typeof(States.ClayDunestrider.XLRecover));
             Loadouts.AddSkill(typeof(States.VoidReaver.KamikazeBlink));
-
             Loadouts.AddSkill(typeof(States.GreaterWisp.WispAmalgamateCharge));
+            //Loadouts.AddSkill(typeof(States.RoboBallBoss.DeploySwarm));
 
             //Skill that does absolutely nothing, useful for getting variants without a certain skill (No teleporting imps/parents)
             emptySkillDef = NewSkillDef(new SerializableEntityStateType(typeof(EmptySkill)), "Body");
@@ -34,8 +34,8 @@ namespace MonsterVariantsPlus.SubClasses.Skills
             hoarderSitDef = NewSkillDef(new SerializableEntityStateType(typeof(States.Scavenger.HoarderSit)), "Body");
             xlRecoverDef = NewSkillDef(new SerializableEntityStateType(typeof(States.ClayDunestrider.XLRecover)), "Body");
             kamikazeBlinkDef = NewSkillDef(new SerializableEntityStateType(typeof(States.VoidReaver.KamikazeBlink)), "Body");
-
-            WispAmalgamateDef = NewSkillDef(new SerializableEntityStateType(typeof(States.GreaterWisp.WispAmalgamateCharge)), "Body");
+            wispAmalgamateDef = NewSkillDef(new SerializableEntityStateType(typeof(States.GreaterWisp.WispAmalgamateCharge)), "Body");
+            //DeploySwarmDef = NewSkillDef(new SerializableEntityStateType(typeof(States.RoboBallBoss.DeploySwarm)), "Weapon");
 
             emptySkillDef.baseMaxStock = 0;
             emptySkillDef.requiredStock = 1000;
@@ -49,8 +49,6 @@ namespace MonsterVariantsPlus.SubClasses.Skills
             xlRecoverDef.baseRechargeInterval = 90f;
             xlRecoverDef.requiredStock = 1;
             xlRecoverDef.stockToConsume = 1;
-
-            
         }
         private static SkillDef NewSkillDef(SerializableEntityStateType state, string stateMachine)
         {
