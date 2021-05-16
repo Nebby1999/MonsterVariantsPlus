ccb78ccdcbd4325eabaa9df0b6a1d72c025130e5
BuildTools <unconfigured@null.spigotmc.org>
2021-05-16 19:01:59 -0400

    Added a Prefab Builder for custom projectiles

---
+++ MonsterVariants+/SubClasses/Prefabs/Projectiles/BrassContraption/MegaBrassBallPrefab.cs
@@ -0,0 +1,26 @@
+﻿using MonsterVariantsPlus.SubClasses.Projectiles;
+using RoR2.Projectile;
+
+using UnityEngine;
+
+namespace MonsterVariantsPlus.SubClasses.Prefabs.Projectiles.BrassContraption
+{
+    public class MegaBrassBallPrefab : PrefabBase<MegaBrassBallPrefab>
+    {
+        public static GameObject prefab;
+        public override void Init()
+        {
+            BuildPrefab();
+            prefab = PrefabObject;
+            CreateProjectilePrefab(PrefabObject);
+        }
+        internal void BuildPrefab()
+        {
+            PrefabObject = InstantiatePrefabClone("Prefabs/Projectiles/BellBall", "MegaBrassBall");
+            PrefabObject.transform.localScale *= 4;
+            ProjectileController ghostPrefab = PrefabObject.GetComponent<ProjectileController>();
+            ghostPrefab.ghostPrefab = InstantiatePrefabClone("prefabs/projectileghosts/BellBallGhost", "MegaBrassBallGhost");
+            ghostPrefab.ghostPrefab.transform.localScale *= 4;
+        }
+    }
+}
